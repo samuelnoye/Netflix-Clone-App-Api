684d97b99a4c65f3caa6811c8c34793d683d28b4
Samuel Noye <samuel.noye@amalitech.org>
2022-04-26 13:57:17 +0000

    Update CollectionViewTableViewCell.swift

--- Netflix Clone App/Views/CollectionViewTableViewCell.swift
+++ Netflix Clone App/Views/CollectionViewTableViewCell.swift
@@ -15,15 +15,30 @@ static let identifier = "CollectionViewTableViewCell"
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
         return collectionView
     }()
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.backgroundColor = .systemPink
+        contentView.addSubview(collectionView)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
     
     required init?(coder: NSCoder) {
         fatalError()
     }
  
+}
+
+extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cel", for: indexPath)
+        
+        return cell
+    }
+    
+    
 }
